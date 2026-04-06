2d4d4d1e99088c0b3930ace06faa835125dd8eee
MagicDrea12 <kiyahl.lopez@gmail.com>
2026-04-06 22:15:14 +0000

    With the tasks array and display method

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    tasks = ["Maths homework", "Physics revision", "CS project"]
+    return render_template("index.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
