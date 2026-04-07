a29c3ff0b5ed6d56c189f154c5c5a19b0fa5b41d
MagicDrea12 <kiyahl.lopez@gmail.com>
2026-04-07 10:36:20 +0000

    Created the Task database

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+# Class Task Design and Database
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
+    duration = db.Column(db.Integer, nullable=False) 
+    deadline = db.Column(db.Integer, nullable=False)
+    priority = db.Column(db.Integer, nullable=False)                   
+    difficulty = db.Column(db.Integer, nullable=False)                
+
+    def __init__(self, u_name, u_duration, u_deadline, u_priority, u_difficulty):
+        self.name = u_name
+        self.duration = (u_duration * 1.2) // 1
+        self.deadline = u_deadline
+        self.priority = u_priority
+        self.difficulty = u_difficulty
+
 
 @app.route("/")
 def home():
-    tasks = ["Maths homework", "Physics revision", "CS project"]
+    tasks = Task.query.all()
     return render_template("index.html", tasks=tasks)
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
